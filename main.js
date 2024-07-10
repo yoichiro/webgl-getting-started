be117292bd88e6df7c7fbc91a25a08e13c2e7074
Yoichiro Tanaka <yoichiro6642@gmail.com>
2024-07-10 16:25:40 +0900

    Initial commit.

---
+++ main.js
@@ -0,0 +1,117 @@
+window.addEventListener('load', () => {
+  const c = document.getElementById('canvas');
+  c.width = 300;
+  c.height = 300;
+
+  const gl = c.getContext('webgl');
+  gl.clearColor(0.0, 0.0, 0.0, 1.0);
+  gl.clearDepth(1.0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+  const vShader = createShader(gl, 'vs');
+  const fShader = createShader(gl, 'fs');
+  const prg = createProgram(gl, vShader, fShader);
+
+  const attLocation = new Array(2);
+  attLocation[0] = gl.getAttribLocation(prg, 'position');
+  attLocation[1] = gl.getAttribLocation(prg, 'color');
+
+  const attStride = new Array(2);
+  attStride[0] = 3;
+  attStride[1] = 4;
+
+  const vertexPosition = [0.0, 1.0, 0.0, 1.0, 0.0, 0.0, -1.0, 0.0, 0.0];
+  const vertexColor = [
+    1.0, 0.0, 0.0, 1.0, 0.0, 1.0, 0.0, 1.0, 0.0, 0.0, 1.0, 1.0,
+  ];
+
+  const positionVBO = createVBO(gl, vertexPosition);
+  const colorVBO = createVBO(gl, vertexColor);
+  setAttribute(gl, [positionVBO, colorVBO], attLocation, attStride);
+
+  const uniLocation = gl.getUniformLocation(prg, 'mvpMatrix');
+
+  const m = new matIV();
+  const mMatrix = m.identity(m.create());
+  const vMatrix = m.identity(m.create());
+  const pMatrix = m.identity(m.create());
+  const tmpMatrix = m.identity(m.create());
+  const mvpMatrix = m.identity(m.create());
+
+  m.lookAt([0.0, 0.0, 3.0], [0, 0, 0], [0, 1, 0], vMatrix);
+  m.perspective(90, c.width / c.height, 0.1, 100, pMatrix);
+  m.multiply(pMatrix, vMatrix, tmpMatrix);
+
+  m.translate(mMatrix, [1.5, 0.0, 0.0], mMatrix);
+
+  m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  m.identity(mMatrix);
+  m.translate(mMatrix, [-1.5, 0.0, 0.0], mMatrix);
+
+  m.multiply(tmpMatrix, mMatrix, mvpMatrix);
+
+  gl.uniformMatrix4fv(uniLocation, false, mvpMatrix);
+  gl.drawArrays(gl.TRIANGLES, 0, 3);
+
+  gl.flush();
+});
+
+const setAttribute = (gl, vbo, attL, attS) => {
+  for (const i in vbo) {
+    gl.bindBuffer(gl.ARRAY_BUFFER, vbo[i]);
+    gl.enableVertexAttribArray(attL[i]);
+    gl.vertexAttribPointer(attL[i], attS[i], gl.FLOAT, false, 0, 0);
+  }
+};
+
+const createShader = (gl, id) => {
+  let shader;
+  const scriptElement = document.getElementById(id);
+  if (!scriptElement) {
+    return undefined;
+  }
+  switch (scriptElement.type) {
+    case 'x-shader/x-vertex':
+      shader = gl.createShader(gl.VERTEX_SHADER);
+      break;
+    case 'x-shader/x-fragment':
+      shader = gl.createShader(gl.FRAGMENT_SHADER);
+      break;
+    default:
+      return undefined;
+  }
+  gl.shaderSource(shader, scriptElement.text);
+  gl.compileShader(shader);
+  if (gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
+    return shader;
+  } else {
+    alert(gl.getShaderInfoLog(shader));
+    return undefined;
+  }
+};
+
+const createProgram = (gl, vs, fs) => {
+  const program = gl.createProgram();
+  gl.attachShader(program, vs);
+  gl.attachShader(program, fs);
+  gl.linkProgram(program);
+  if (gl.getProgramParameter(program, gl.LINK_STATUS)) {
+    gl.useProgram(program);
+    return program;
+  } else {
+    alert(gl.getProgramInfoLog(program));
+    return undefined;
+  }
+};
+
+const createVBO = (gl, data) => {
+  const vbo = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, vbo);
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(data), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, null);
+  return vbo;
+};
